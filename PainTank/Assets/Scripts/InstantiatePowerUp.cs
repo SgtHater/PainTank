1982a58264ed3c00a0ebed39f67e9dfee5be5eaf
Scr4py <kevinholst@gmx.net>
2015-08-26 14:00:47 +0200

    [NEW] PowerUP Skripts, UI added with 4 panels

---
+++ PainTank/Assets/Scripts/InstantiatePowerUp.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class InstantiatePowerUp : MonoBehaviour {
+    public GameObject Wall;
+    public GameObject Shield;
+    public GameObject Nitro;
+    public GameObject Laser;
+    public GameObject Map;
+
+    public int pooledamount = 1;
+
+    private List<GameObject> powerups;
+	// Use this for initialization
+	void Start ()
+    {
+        powerups = new List<GameObject>();
+        for (int i = 0; i < pooledamount; i++)
+        {
+            GameObject wall = (GameObject)Instantiate(Wall);
+            wall.SetActive(false);
+            powerups.Add(wall);
+            GameObject shield = (GameObject)Instantiate(Shield);
+            shield.SetActive(false);
+            powerups.Add(shield);
+            GameObject nitro = (GameObject)Instantiate(Nitro);
+            nitro.SetActive(false);
+            powerups.Add(nitro);
+            GameObject laser = (GameObject)Instantiate(Laser);
+            laser.SetActive(false);
+            powerups.Add(laser);
+        }
+
+        InvokeRepeating("InstantiatePowerUps", 2f, 15f);
+	}
+	
+    void InstantiatePowerUps()
+    {
+        int rnd = Random.Range(0, powerups.Count);
+
+        if (rnd == 0)
+        {
+            foreach (var item in powerups)
+            {
+                if (!item.activeInHierarchy)
+                {
+                    if (item.name.StartsWith("Wall"))
+                    {
+                        Vector3 rdnTransform = new Vector3(Random.Range(360, 436), 146, Random.Range(-66, -144));
+                        item.SetActive(true);
+                        item.transform.position = rdnTransform;
+                        item.transform.rotation = transform.rotation;
+
+                    }
+                }
+            }
+        }
+
+        if (rnd == 1)
+        {
+            foreach (var item in powerups)
+            {
+                if (!item.activeInHierarchy)
+                {
+                    if (item.name.StartsWith("Shield"))
+                    {
+                        Vector3 rdnTransform = new Vector3(Random.Range(360, 436), 146, Random.Range(-66, -144));
+                        item.SetActive(true);
+                        item.transform.position = rdnTransform;
+                        item.transform.rotation = transform.rotation;
+
+                    }
+                }
+            }
+        }
+
+        if (rnd == 2)
+        {
+            foreach (var item in powerups)
+            {
+                if (!item.activeInHierarchy)
+                {
+                    if (item.name.StartsWith("Nitro"))
+                    {
+                        Vector3 rdnTransform = new Vector3(Random.Range(360, 436), 146, Random.Range(-66, -144));
+                        item.SetActive(true);
+                        item.transform.position = rdnTransform;
+                        item.transform.rotation = transform.rotation;
+
+                    }
+                }
+            }
+        }
+
+        if (rnd == 3)
+        {
+            foreach (var item in powerups)
+            {
+                if (!item.activeInHierarchy)
+                {
+                    if (item.name.StartsWith("Laser"))
+                    {
+                        Vector3 rdnTransform = new Vector3(Random.Range(360, 436), 146, Random.Range(-66, -144));
+                        item.SetActive(true);
+                        item.transform.position = rdnTransform;
+                        item.transform.rotation = transform.rotation;
+
+                    }
+                }
+            }
+        }
+    }
+}
+
---
+++ PainTank/Assets/Scripts/PowerUpDestroy.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerUpDestroy : MonoBehaviour
+{
+    void OnEnable()
+    {
+        Invoke("Destroy", 5f);
+    }
+
+    void Destroy()
+    {
+        gameObject.SetActive(false);
+    }
+
+    void OnDisable()
+    {
+        CancelInvoke();
+    }
+
+}
