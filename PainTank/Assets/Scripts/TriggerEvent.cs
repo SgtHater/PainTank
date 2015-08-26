2403a698020435b79e20b0a9c1c9b31d2d667d12
Scr4py <kevinholst@gmx.net>
2015-08-26 17:48:26 +0200

    [CREATE] TriggerEventscripts, Prefabs, UIController Script, [Changed] UI Scene, Prefabs, TagManager

---
+++ PainTank/Assets/Scripts/TriggerEvent.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TriggerEvent : MonoBehaviour
+{
+    public GameObject controller;
+    public GameObject Prefab;
+
+    void Awake()
+    {
+        controller = GameObject.Find("UIController");
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "JackEagle")
+        {
+            if (this.controller.GetComponent<UiController>().PanelArray[0].childCount <= 3)
+            {
+                GameObject powerup = Instantiate(Prefab) as GameObject;
+                powerup.transform.SetParent(this.controller.GetComponent<UiController>().PanelArray[0]);
+                this.gameObject.SetActive(false);
+            }
+        }
+    }
+
+}
---
+++ PainTank/Assets/Scripts/UiController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UiController : MonoBehaviour
+{
+    public RectTransform[] PanelArray;
+
+    void Start()
+    {
+        
+    }
+
+}
