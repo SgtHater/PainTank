1d4804a2a462bff0a488e42329fe358cd63ec8a7
Michaeel B <luke-20@live.de>
2015-08-27 15:49:49 +0200

    [NEW] Finished Tank Fire Controls and Movement.. [CHANGED] Tank Prefab

---
+++ PainTank/Assets/Scripts/BulletController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletController : MonoBehaviour
+{
+
+    public void OnCollisionEnter(Collision other)
+    {
+        Invoke("Destroy", 3f);
+    }
+
+    void Destroy()
+    {
+        this.transform.position = Vector3.zero;
+        this.transform.rotation = Quaternion.identity;
+        this.gameObject.SetActive(false);
+    }
+}
--- PainTank/Assets/Scripts/ShootController.cs
+++ PainTank/Assets/Scripts/ShootController.cs
@@ -5,12 +5,36 @@ public class ShootController : MonoBehaviour
 {
     public GameObject ParentTank;
     public FireController fireController;
+    public float delay = 3;
+    bool fired = false;
 
     void Update()
     {
+        FireDelay();
+        ActivateTimer();
+    }
+    void FireDelay()
+    {
         if (Input.GetAxis(ParentTank.name + "_Fire") != 0)
         {
-            fireController.ActivateBullet(this.transform);
+            if (!fired)
+            {
+                fireController.ActivateBullet(this.transform);
+                fired = true;
+                ActivateTimer();
+            }
+        }
+    }
+    void ActivateTimer()
+    {
+        if (fired)
+        {
+            delay -= Time.deltaTime;
+            if (delay <= 0)
+            {
+                fired = false;
+                delay = 3.5f;
+            }
         }
     }
 }
